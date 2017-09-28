98821c9084c0a99a2db1b8a29e07c081f46a03df
[tiger] <[645189097@qq.com]>
2017-09-28 09:10:24 +0800

    work_1.0.1

---
+++ js/babyUrl.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2017/9/25.
+ */
+/*//宝宝头像拼接
+* saids:宝宝id
+* */
+babyurl = att_get_css(saids, "message/patient", "vdaifu_head");
---
+++ js/doctorUrl.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/9/25.
+ */
+/*
+* 医生头像拼接:
+* said:医生的ID
+* */
+sheadurl = att_get_css(said, "doctor", "vdaifu_head");
